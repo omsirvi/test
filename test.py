fbe27beda6f5fb68bbeb69966970f3c3784cae17
Om <omsirvi.prakash@gmail.com>
2018-07-20 18:59:57 +0530

    update file

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ from bs4 import BeautifulSoup
 def run():
     page = requests.get('http://www.ecourts.gov.in/ecourts_home/')
     soup = BeautifulSoup(page.text, 'html.parser')
-    print(soup.a)
+    print(soup.get_text())
+    fileopen = open('index.html', 'w' )
+    fileopen.write(soup.get_text())
+    fileopen.close()
 run()
+
